764e5919c6d2c6773b8c5efb783e781e6416592e
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2020-12-01 21:44:59 +0800

    f

--- layers/optimizer.py
+++ layers/optimizer.py
@@ -1,8 +1,13 @@
-
-
-
-
-
+#! /usr/bin/env python
+# coding=utf-8
+# ================================================================
+#
+#   Author      : miemie2013
+#   Created date: 2020-07-05 13:35:27
+#   Description : 《纯python实现一个深度学习框架》课程源码
+#
+# ================================================================
+import numpy as np
 
 
 class Optimizer(object):
@@ -37,6 +42,18 @@ class SGD(Optimizer):
             # 推导完成。
             keep = (1.0 - lr * decay_coeff)   # keep通常是0.999...这样的值，即param先乘以一个0.999...的值进行衰减再减去lr * dparam。
             param = keep * param - lr * dparam
+        elif decay_type == 'L1Decay':
+            # L2正则化，即在损失函数上加上该参数的绝对值项：
+            # loss_new = loss + decay_coeff * |param|
+            # loss_new对param求偏导：
+            # dloss_new = dloss + decay_coeff * sign(param)    (dloss即本方法的形参dparam)
+            #           = dparam + decay_coeff * sign(param)
+            # 所以权重更新公式为
+            # param = param - lr * dloss_new
+            #       = param - lr * (dparam + decay_coeff * sign(param))
+            #       = param - lr * dparam - lr * decay_coeff * sign(param)
+            # 推导完成。相当于不使用正则化时param再加上或减去一个极小的正实数lr * decay_coeff。
+            param = param - lr * dparam - lr * decay_coeff * np.sign(param)
         return param
 
 
