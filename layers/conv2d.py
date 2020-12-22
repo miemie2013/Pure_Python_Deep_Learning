0b6b64bc5e44352052e53ed88ccb7798a106d2fc
miemie2013 <53960695+miemie2013@users.noreply.github.com>
2020-12-22 23:04:39 +0800

    grad

--- layers/conv2d.py
+++ layers/conv2d.py
@@ -11,6 +11,36 @@ import numpy as np
 from layers.base_layer import Layer
 
 
+def F_conv2d(x, w, b, stride, padding):
+    # 卷积层推理时的前向传播。
+    N, C, H, W = x.shape
+    out_C, in_C, kH, kW = w.shape
+    assert (C == in_C), "x.shape[1] must equal in_C."
+    out_W = (W+2*padding-(kW-1)) // stride
+    out_H = (H+2*padding-(kH-1)) // stride
+    out = np.zeros((N, out_C, out_H, out_W), np.float32)
+
+    # 1.先对图片x填充得到填充后的图片pad_x
+    pad_x = np.zeros((N, C, H + padding*2, W + padding*2), np.float32)
+    pad_x[:, :, padding:padding + H, padding:padding + W] = x
+
+    # 2.卷积核滑动，只会在H和W两个方向上滑动
+    for i in range(out_H):   # i是纵坐标
+        for j in range(out_W):   # j是横坐标
+            ori_x = j*stride   # 卷积核在pad_x中的横坐标，等差数列，公差是stride
+            ori_y = i*stride   # 卷积核在pad_x中的纵坐标，等差数列，公差是stride
+            part_x = pad_x[:, :, ori_y:ori_y+kH, ori_x:ori_x+kW]   # 截取卷积核所处的位置的像素 [N, in_C, kH, kW]
+            exp_part_x = np.expand_dims(part_x, 1)   # 增加1维，[N, 1,     in_C, kH, kW]。
+            exp_w = np.expand_dims(w, 0)      # 卷积核也增加1维，[1, out_C, in_C, kH, kW]。
+            mul = exp_part_x * exp_w   # 卷积核和exp_part_x相乘，[N, out_C, in_C, kH, kW]。
+            mul = np.sum(mul, axis=(2, 3, 4))       # 后3维求和，[N, out_C]。
+            if b is not None:
+                mul += b    # 加上偏移，[N, out_C]。
+            # 将得到的新像素写进out的对应位置
+            out[:, :, i, j] = mul
+    return out
+
+
 class Conv2D(Layer):
     def __init__(self,
                  in_C,
@@ -69,6 +99,11 @@ class Conv2D(Layer):
         assert (C == in_C), "x.shape[1] must equal in_C."
         out_W = (W+2*padding-(kW-1)) // stride
         out_H = (H+2*padding-(kH-1)) // stride
+
+        # 针对4x4卷积核
+        if kW == 4 and stride == 2 and padding == 1:
+            out_H += 1
+            out_W += 1
         out = np.zeros((N, out_C, out_H, out_W), np.float32)
 
         # 1.先对图片x填充得到填充后的图片pad_x
@@ -159,6 +194,140 @@ class Conv2D(Layer):
         w = optimizer.update(self.name+'_weight', w, dW, self.w_lr, self.w_decay_type, self.w_decay)
         self.w = w
         # loss对输入x的偏导数，用来更新前面的层的权重
+        dx = dpad_x[:, :, padding:padding + H, padding:padding + W]
+        return dx
+
+    def get_grad2(self, grad, optimizer):
+        '''
+        对本层的权重求偏导，以更新本层的权重。对本层的输入x求偏导，以更新前面的层的权重。
+        设本层的权重是w，若loss = f(a, b, c, ...) = a(w)+b(w)+c(w)+...，那么loss对w的偏导数(偏导符号打不出来，用d表示了)
+        dloss/dw = dloss/da * da/dw + dloss/db * db/dw + dloss/dc * dc/dw + ...
+        a, b, c是中间变量，在这一层，self.output里的所有元素就是这个例子里的a, b, c, ...是中间变量，
+        而dloss/da, dloss/db, dloss/dc, ...即grad里的所有元素。所以我们只需要求出
+        da/dw, db/dw, dc/dw, ...就可以求出dloss/dw了！ 求loss对本层输入的偏导数，同理。
+        '''
+        x, out = self.input, self.output   # 获取训练时前向传播的输入输出
+        N, C, H, W = x.shape
+        N, out_C, out_H, out_W = out.shape
+        out_C, in_C, kH, kW = self.w.shape
+        w = self.w
+        b = self.b
+        stride = self.stride
+        padding = self.padding
+
+        # 对图片x填充得到填充后的图片pad_x
+        pad_x = np.zeros((N, C, H + padding*2, W + padding*2), np.float32)
+        pad_x[:, :, padding:padding + H, padding:padding + W] = x
+
+        # loss对pad_x的偏导数
+        dpad_x = np.zeros(pad_x.shape, np.float32)   # [N, C, H + padding*2, W + padding*2]
+        # 卷积核滑动，只会在H和W两个方向上滑动
+        for i in range(out_H):   # i是纵坐标
+            for j in range(out_W):   # j是横坐标
+                ori_x = j*stride   # 卷积核在pad_x中的横坐标，等差数列，公差是stride
+                ori_y = i*stride   # 卷积核在pad_x中的纵坐标，等差数列，公差是stride
+
+                # 获取loss对当前位置输出元素的偏导数dy。
+                dy = grad[:, :, i:i+1, j:j+1]  # [N, out_C, 1, 1]  固定前2维为特定下标，就是课件里的标量dloss/dy_nkij了！
+                dy = np.expand_dims(dy, 2)   # 增加1维，[N, out_C, 1, 1, 1]。
+
+                # loss对输入x的偏导数
+                exp_W = np.expand_dims(w, 0)         # [1, out_C, in_C, kH, kW]
+                temp = dy * exp_W                    # [N, out_C, in_C, kH, kW]
+                temp = np.sum(temp, axis=(1, ))      # [N, in_C, kH, kW]  全连接层中，是把偏移数量那一维求和掉，卷积层里也是一样，把偏移数量那一维求和掉。
+                dpad_x[:, :, ori_y:ori_y+kH, ori_x:ori_x+kW] += temp    # [N, in_C, H + padding*2, W + padding*2]
+        dx = dpad_x[:, :, padding:padding + H, padding:padding + W]
+        return dx
+
+    def grid_pad(self, x, pad, padding_l, padding_t, target_h, target_w):
+        # x: [N, in_C, out_H, out_W]
+        N, in_C, out_H, out_W = x.shape
+        if pad == 0:  # stride == 1
+            pad_x = x
+            in_H = out_H
+            in_W = out_W
+        elif pad == 1:  # stride == 2
+            mask = np.array([1, 0], np.float32)
+            mask = np.tile(mask, out_W)
+            zeros = np.zeros(mask.shape, np.float32)
+            mask = np.reshape(mask, (1, -1))
+            zeros = np.reshape(zeros, (1, -1))
+            mask = np.concatenate([mask, zeros], 0)
+            mask = np.tile(mask, [out_H, 1])   # [in_H, in_W]
+            in_H = mask.shape[0]
+            in_W = mask.shape[1]
+            mask = np.reshape(mask, (1, 1, in_H, in_W))   # [1, 1, in_H, in_W]
+            # 模拟最近邻插值
+            pad_x = np.zeros((N, in_C, in_H, in_W), np.float32)
+            for i in range(out_H):
+                for j in range(out_W):
+                    pad_x[:, :, i*2, j*2] = x[:, :, i, j]
+                    pad_x[:, :, i*2, j*2+1] = x[:, :, i, j]
+                    pad_x[:, :, i*2+1, j*2] = x[:, :, i, j]
+                    pad_x[:, :, i*2+1, j*2+1] = x[:, :, i, j]
+            pad_x = pad_x * mask
+        elif pad == 2:  # stride > 2的情况基本不会出现，所以不实现
+            # mask = np.array([1, 0, 0], np.float32)
+            pass
+        padding_r = target_w - in_W - padding_l
+        padding_b = target_h - in_H - padding_t
+        if padding_b > 0:
+            zeros_b = np.zeros((N, in_C, padding_b, in_W), np.float32)
+            pad_x = np.concatenate([pad_x, zeros_b], 2)
+        if padding_t > 0:
+            zeros_t = np.zeros((N, in_C, padding_t, in_W), np.float32)
+            pad_x = np.concatenate([zeros_t, pad_x], 2)
+        # 针对4x4卷积核
+        if padding_b < 0:
+            pad_x = pad_x[:, :, :target_h, :]
+
+        if padding_r > 0:
+            zeros_r = np.zeros((N, in_C, target_h, padding_r), np.float32)
+            pad_x = np.concatenate([pad_x, zeros_r], 3)
+        if padding_l > 0:
+            zeros_l = np.zeros((N, in_C, target_h, padding_l), np.float32)
+            pad_x = np.concatenate([zeros_l, pad_x], 3)
+        # 针对4x4卷积核
+        if padding_r < 0:
+            pad_x = pad_x[:, :, :, :target_w]
+
+        return pad_x
+
+    def get_grad(self, grad, optimizer):
+        '''
+        对本层的权重求偏导，以更新本层的权重。对本层的输入x求偏导，以更新前面的层的权重。
+        设本层的权重是w，若loss = f(a, b, c, ...) = a(w)+b(w)+c(w)+...，那么loss对w的偏导数(偏导符号打不出来，用d表示了)
+        dloss/dw = dloss/da * da/dw + dloss/db * db/dw + dloss/dc * dc/dw + ...
+        a, b, c是中间变量，在这一层，self.output里的所有元素就是这个例子里的a, b, c, ...是中间变量，
+        而dloss/da, dloss/db, dloss/dc, ...即grad里的所有元素。所以我们只需要求出
+        da/dw, db/dw, dc/dw, ...就可以求出dloss/dw了！ 求loss对本层输入的偏导数，同理。
+        '''
+        x, out = self.input, self.output   # 获取训练时前向传播的输入输出
+        N, C, H, W = x.shape
+        N, out_C, out_H, out_W = out.shape
+        out_C, in_C, kH, kW = self.w.shape
+        w = self.w   # [out_C, in_C, kH, kW]
+        b = self.b
+        stride = self.stride
+        padding = self.padding
+
+        w_t = np.reshape(w, (out_C, in_C*kH*kW))   # [out_C, in_C*kH*kW]
+        w_t = w_t.transpose(1, 0)   # [in_C*kH*kW, out_C]
+        w_t = np.reshape(w_t, (in_C*kH*kW, out_C, 1, 1))   # [in_C*kH*kW, out_C, 1, 1]
+        dx = F_conv2d(grad, w_t, b=None, stride=1, padding=0)   # [N, in_C*kH*kW, out_H, out_W]
+        dx = np.reshape(dx, (-1, in_C, kH, kW, out_H, out_W))   # [N, in_C, kH, kW, out_H, out_W]
+
+        # loss对pad_x的偏导数
+        dpad_x = np.zeros((N, C, H + padding*2, W + padding*2), np.float32)   # [N, C, H + padding*2, W + padding*2]
+
+        # 遍历卷积核每一个焦点
+        target_h = H + padding*2
+        target_w = W + padding*2
+        for i in range(kH):
+            for j in range(kW):
+                temp = dx[:, :, i, j, :, :]    # [N, in_C, out_H, out_W]
+                pad_dx = self.grid_pad(temp, stride-1, padding_l=j, padding_t=i, target_h=target_h, target_w=target_w)
+                dpad_x += pad_dx    # [N, in_C, H + padding*2, W + padding*2]
         dx = dpad_x[:, :, padding:padding + H, padding:padding + W]
         return dx
 
---
+++ test/03_test_conv_grad.py
@@ -0,0 +1,175 @@
+#! /usr/bin/env python
+# coding=utf-8
+# ================================================================
+#
+#   Author      : miemie2013
+#   Created date: 2020-07-05 13:35:27
+#   Description : 《纯python实现一个深度学习框架》课程源码
+#
+# ================================================================
+import datetime
+import json
+from collections import deque
+import paddle.fluid as fluid
+import paddle.fluid.layers as P
+import sys
+import time
+import shutil
+import math
+import copy
+import random
+import threading
+import numpy as np
+import os
+from paddle.fluid.param_attr import ParamAttr
+from paddle.fluid.regularizer import L2Decay
+
+from layers.fc import FC
+from layers.conv2d import Conv2D
+from layers.bn import BatchNorm
+from layers.loss import MSELoss
+from layers.optimizer import *
+
+
+import paddle
+paddle.enable_static()
+
+if __name__ == '__main__':
+    use_gpu = False
+
+    lr = 0.1
+
+    startup_prog = fluid.Program()
+    train_prog = fluid.Program()
+    with fluid.program_guard(train_prog, startup_prog):
+        with fluid.unique_name.guard():
+            inputs = P.data(name='input_1', shape=[-1, 3, 28, 28], append_batch_size=False, dtype='float32')
+            conv01_out_tensor = fluid.layers.conv2d(input=inputs, num_filters=7, filter_size=1, stride=1, padding=0,
+                                                    param_attr=ParamAttr(name="conv01_weights"),
+                                                    bias_attr=ParamAttr(name="conv01_bias"))
+            conv02_out_tensor = fluid.layers.conv2d(input=conv01_out_tensor, num_filters=8, filter_size=4, stride=2, padding=1,
+                                                    param_attr=ParamAttr(name="conv02_weights"),
+                                                    bias_attr=ParamAttr(name="conv02_bias"))
+
+            grad = fluid.gradients(conv02_out_tensor, conv01_out_tensor, no_grad_set=None)[0]
+
+
+            # 建立损失函数
+            y_true = P.data(name='y_true', shape=[-1, 8, 14, 14], append_batch_size=False, dtype='float32')
+            # 先把差值逐项平方，可以用P.pow()这个op，也可以用python里的运算符**。
+            mseloss = P.pow(y_true - conv02_out_tensor, 2)
+            mseloss = P.reduce_mean(mseloss)       # 再求平均，即mse损失函数
+
+            # 优化器，选SGD
+            optimizer = fluid.optimizer.SGD(learning_rate=lr)
+            optimizer.minimize(mseloss)
+
+
+    eval_prog = fluid.Program()
+    with fluid.program_guard(eval_prog, startup_prog):
+        with fluid.unique_name.guard():
+            # 重新建立一次网络，用相同的张量名，不用写损失层
+            inputs = P.data(name='input_1', shape=[-1, 3, 28, 28], append_batch_size=False, dtype='float32')
+            conv01_out_tensor = fluid.layers.conv2d(input=inputs, num_filters=7, filter_size=1, stride=1, padding=0,
+                                                    param_attr=ParamAttr(name="conv01_weights"),
+                                                    bias_attr=ParamAttr(name="conv01_bias"))
+            conv02_out_tensor = fluid.layers.conv2d(input=conv01_out_tensor, num_filters=8, filter_size=4, stride=2, padding=1,
+                                                    param_attr=ParamAttr(name="conv02_weights"),
+                                                    bias_attr=ParamAttr(name="conv02_bias"))
+            eval_fetch_list = [conv01_out_tensor, conv02_out_tensor]
+    eval_prog = eval_prog.clone(for_test=True)
+    # 参数初始化
+    gpu_id = int(os.environ.get('FLAGS_selected_gpus', 0))
+    place = fluid.CUDAPlace(gpu_id) if use_gpu else fluid.CPUPlace()
+    exe = fluid.Executor(place)
+    exe.run(startup_prog)
+    compiled_eval_prog = fluid.compiler.CompiledProgram(eval_prog)
+
+
+    # 纯python搭建的神经网络的权重。初始值是paddle相同层的初始值。为了模拟paddle训练过程。
+    # 1.卷积层
+    paddle_conv01_weights = np.array(fluid.global_scope().find_var('conv01_weights').get_tensor())
+    paddle_conv01_bias = np.array(fluid.global_scope().find_var('conv01_bias').get_tensor())
+    # 2.卷积层
+    paddle_conv02_weights = np.array(fluid.global_scope().find_var('conv02_weights').get_tensor())
+    paddle_conv02_bias = np.array(fluid.global_scope().find_var('conv02_bias').get_tensor())
+    # 3.损失函数层，没有权重。
+
+    #  纯python搭建的神经网络
+    conv01 = Conv2D(3, num_filters=7, filter_size=1, stride=1, padding=0, use_bias=True, name='conv01')
+    conv02 = Conv2D(7, num_filters=8, filter_size=4, stride=2, padding=1, use_bias=True, name='conv02')
+    mse01 = MSELoss()
+    optimizer2 = SGD(lr=lr)
+    # 初始化自己网络的权重
+    conv01.init_weights(paddle_conv01_weights, paddle_conv01_bias)
+    conv02.init_weights(paddle_conv02_weights, paddle_conv02_bias)
+
+
+    # 只训练8步
+    for step in range(8):
+        print('------------------ step %d ------------------' % step)
+        # ==================== train ====================
+        batch_data = np.random.normal(loc=0, scale=1, size=(2, 3, 28, 28)).astype(np.float32)
+        y_true_arr = np.random.normal(loc=0, scale=1, size=(2, 8, 14, 14)).astype(np.float32)
+
+        paddle_mseloss_out, paddle_conv01_out, paddle_conv02_out, grad222 = exe.run(train_prog, feed={"input_1": batch_data, "y_true": y_true_arr, },
+                                                                       fetch_list=[mseloss, conv01_out_tensor, conv02_out_tensor, grad])
+
+        print('train_forward:')
+        # python代码模拟训练过程，与paddle的输出校验。我们希望和飞桨有相同的输出。
+        my_conv01_out = conv01.train_forward(batch_data)
+        my_conv02_out = conv02.train_forward(my_conv01_out)
+        my_mseloss_out = mse01.train_forward(my_conv02_out, y_true_arr)
+
+
+        diff_conv01_out = np.sum((paddle_conv01_out - my_conv01_out)**2)
+        print('diff_conv01_out=%.6f' % diff_conv01_out)   # 若是0，则表示成功模拟出PaddlePaddle的输出结果
+        diff_conv02_out = np.sum((paddle_conv02_out - my_conv02_out)**2)
+        print('diff_conv02_out=%.6f' % diff_conv02_out)   # 若是0，则表示成功模拟出PaddlePaddle的输出结果
+        diff_mseloss_out = np.sum((paddle_mseloss_out - my_mseloss_out)**2)
+        print('diff_mseloss_out=%.6f' % diff_mseloss_out)   # 若是0，则表示成功模拟出PaddlePaddle bn层的输出结果
+
+        print('\ntrain_backward:')
+        # 纯python搭建的神经网络进行反向传播啦！求偏导数即可。反向传播会更新权重，我们期望和飞桨有相同的权重。
+        my_conv02_out_grad = mse01.train_backward(optimizer2)
+
+        # 求卷积层conv02的输出对其输入的偏导数。输出先对自己求偏导得1
+        dy = np.ones(my_conv02_out_grad.shape)
+        grad22 = conv02.get_grad(dy, optimizer2)
+        diff_grad = np.sum((grad22 - grad222)**2)
+        print('diff_grad=%.6f' % diff_grad)
+
+
+        my_conv01_out_grad = conv02.train_backward(my_conv02_out_grad, optimizer2)
+        inputs_grad = conv01.train_backward(my_conv01_out_grad, optimizer2)
+
+        # 和飞桨更新后的权重校验。
+        paddle_conv01_weights = np.array(fluid.global_scope().find_var('conv01_weights').get_tensor())
+        paddle_conv01_bias = np.array(fluid.global_scope().find_var('conv01_bias').get_tensor())
+        paddle_conv02_weights = np.array(fluid.global_scope().find_var('conv02_weights').get_tensor())
+        paddle_conv02_bias = np.array(fluid.global_scope().find_var('conv02_bias').get_tensor())
+
+
+        diff_conv02_weights = np.sum((paddle_conv02_weights - conv02.w)**2)
+        print('diff_conv02_weights=%.6f' % diff_conv02_weights)   # 若是0，则表示成功模拟出权重更新
+        diff_conv02_bias = np.sum((paddle_conv02_bias - conv02.b)**2)
+        print('diff_conv02_bias=%.6f' % diff_conv02_bias)   # 若是0，则表示成功模拟出权重更新
+        diff_conv01_weights = np.sum((paddle_conv01_weights - conv01.w)**2)
+        print('diff_conv01_weights=%.6f' % diff_conv01_weights)   # 若是0，则表示成功模拟出权重更新
+        diff_conv01_bias = np.sum((paddle_conv01_bias - conv01.b)**2)
+        print('diff_conv01_bias=%.6f' % diff_conv01_bias)   # 若是0，则表示成功模拟出权重更新
+
+        # ==================== test ====================
+        test_data = np.random.normal(loc=0, scale=1, size=(2, 3, 28, 28)).astype(np.float32)
+        paddle_test_conv01_out, paddle_test_conv02_out = exe.run(compiled_eval_prog, feed={"input_1": test_data, }, fetch_list=eval_fetch_list)
+        # 自己网络的test
+        print('\ntest_forward:')
+        my_test_conv01_out = conv01.test_forward(test_data)
+        my_test_conv02_out = conv02.test_forward(my_test_conv01_out)
+        diff_test_conv01_out = np.sum((paddle_test_conv01_out - my_test_conv01_out)**2)
+        print('diff_test_conv01_out=%.6f' % diff_test_conv01_out)   # 若是0，则表示成功模拟出推理
+        diff_test_conv02_out = np.sum((paddle_test_conv02_out - my_test_conv02_out)**2)
+        print('diff_test_conv02_out=%.6f' % diff_test_conv02_out)   # 若是0，则表示成功模拟出推理
+
+
+
